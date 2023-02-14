a977deea7f1632e57d129f93752be5f70fb0b4e5
Xiyue Wang <59850013+XYWayne@users.noreply.github.com>
2023-02-13 17:18:56 -0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '闹闹',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o4uVg6dC5yR9sQ4k-kuZqAgHM0bU',
+      id: 'o4uVg6RmDOadjY1ftzQoseQ9QUKI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'ahD3JBM9VD244Cfc3--51CZ3BBwoWMuQySsKJtZ7VKc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
