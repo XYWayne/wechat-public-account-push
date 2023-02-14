471a3b419939572c032c09c3832e17e9313425e2
Xiyue Wang <59850013+XYWayne@users.noreply.github.com>
2023-02-13 17:01:05 -0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o4uVg6dC5yR9sQ4k-kuZqAgHM0bU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'ZdKQdhl5Vzua9O2NFbXAlnIJ5Eenzf-5O-jB_BfKRw0',
+      useTemplateId: 's36p1g0bRxrniaeoNM__d09WPmvxEjR2A_M1ubu21Jk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-04',
       festivals: [
