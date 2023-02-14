5c62a45f9b32cc1b9c7a01e084cc9a4bb4cce4b1
Xiyue Wang <59850013+XYWayne@users.noreply.github.com>
2023-02-13 16:27:10 -0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -1,8 +1,11 @@
 /* eslint-disable */
 const TEMPLATE_CONFIG = [
   {
+    // 自定义，随便怎么填，与config里的templateId对应
     id: '0001',
+    // 模板标题，会展示到推送消息的标题位置
     title: '亲爱的, 早上好',
+    // 模板内容，支持部分markdown语法
     desc: `
       **{{date.DATA}}**
       
