9588f00828a1e688c1cc3ceb7bae70609efa922f
Xiyue Wang <59850013+XYWayne@users.noreply.github.com>
2023-02-13 16:25:24 -0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -33,7 +33,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '李四', year: '2001', date: '07-17',
         },
         {
           type: '节日', name: '纪念日', year: '2019', date: '05-11',
